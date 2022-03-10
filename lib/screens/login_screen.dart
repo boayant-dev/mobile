07dae74b24c799387fa0292f73c6d4e99fba55b9
Rerecodes <79289462+Rerecodes@users.noreply.github.com>
2022-03-10 09:32:52 +0100

    Fixed naming convention for loginScreen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LogIn extends StatelessWidget {
-  const LogIn({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
